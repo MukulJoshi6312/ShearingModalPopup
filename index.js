6cff45fdf96d88cf2266e7778db0ab160d4a1c01
Mukul joshi <80614782+MukulJoshi6312@users.noreply.github.com>
2023-03-26 12:54:43 +0530

    commit
    Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+// modal open function
+const openModal = () => {
+    console.log('modal open')
+    modal.classList.add('active');
+    overlay.classList.add('overlayactive');
+}
+
+const closeModal = () => {
+    console.log('modal close');
+    modal.classList.remove('active');
+    overlay.classList.remove('overlayactive');
+}
